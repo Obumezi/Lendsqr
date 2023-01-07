d464da0d823b8b0ac94308e94aac432737d9f704
Obumneme Eziamaka <eziamakaobum@yahoo.co.uk>
2023-01-07 10:12:30 +0100

    Sidebar Alingned and icons added

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,49 +1,114 @@
 import '../Styles/Sidebar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import {
+    faHome, faUser,
+    faUsers,
+    faMoneyBill, faHandshake, faPiggyBank,
+    faHandHoldingDollar, faUserCheck, faUserXmark, faBriefcase, faBank
+} from '@fortawesome/free-solid-svg-icons'
+
+
 
 function Sidebar() {
     return (
         <aside className="sidebar">
+
             <h2 className="sidebar__title">Switch Organisation</h2>
-            <FontAwesomeIcon className='coffee' icon={faCoffee} />
+
+
+
+            <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faHome} />
+                <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>DashBoard</a>
+            </div>
+
+
+
+
+
             <ul className="sidebar__menu">
+                <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                    <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faUser} />
+                    <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Users</a>
+                </div>
+
+
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">DashBoard</a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faUsers} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Guarantors</a>
+                    </div>
+
+                </li>
+
+
+
+
+
+
+
+
+                <li className="sidebar__menu-item">
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faMoneyBill} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Loans</a>
+                    </div>
+
+                </li>
+
+
+                <li className="sidebar__menu-item">
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faHandshake} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Decison Model</a>
+                    </div>
+
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Users</a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faPiggyBank} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Savings</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Loans </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faHandHoldingDollar} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Loan Request</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Decison Models </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faUserCheck} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>WhiteList</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Savings </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faUserXmark} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Karma</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Loan Requests </a>
+                    <a href="#" className="sidebar__menu-link-head">BUSINESSES </a>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Whitelist </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faBriefcase} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Organisation</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Karma </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faHandHoldingDollar} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Loan Products</a>
+                    </div>
                 </li>
                 <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link-head">Business** </a>
+                    <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                        <FontAwesomeIcon style={{ verticalAlign: 'middle' }} icon={faBank} />
+                        <a href="#" className="sidebar__menu-link" style={{ display: 'inline-block', verticalAlign: 'middle' }}>Savings Products</a>
+                    </div>
                 </li>
-                <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Organization </a>
-                </li>
-                <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Loan Products </a>
-                </li>
-                <li className="sidebar__menu-item">
-                    <a href="#" className="sidebar__menu-link">Fees and Charges </a>
-                </li> 
                 <li className="sidebar__menu-item">
                     <a href="#" className="sidebar__menu-link">Transactions </a>
                 </li>
